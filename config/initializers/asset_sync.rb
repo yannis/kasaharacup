b0fc9671a778b83928e182a078d427ae475e33c8
Yannis Jaquet <yannis.jaquet@unige.ch>
2014-05-15 21:00:25 +0200

    corrects assets_sync env vars

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
   AssetSync.configure do |config|
-    config.fog_provider = 'AWS'
-    config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-    config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-    config.fog_directory = ENV['FOG_DIRECTORY']
+    config.fog_provider = ENV['fog_provider']
+    config.aws_access_key_id = ENV['aws_access_key_id']
+    config.aws_secret_access_key = ENV['aws_secret_access_key']
+    config.fog_directory = ENV['fog_directory']
 
     # Increase upload performance by configuring your region
-    config.fog_region = ENV['FOG_REGION']
+    config.fog_region = ENV['fog_region']
     #
     # Don't delete files from the store
     config.existing_remote_files = "ignore"
