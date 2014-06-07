7bd976b828616a62673f5d7fdbde10ad7fe00ce6
Yannis Jaquet <yannis.jaquet@unige.ch>
2014-06-07 21:48:24 +0200

    corrects a bug affecting kenshi validation of participations

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -5,8 +5,8 @@ class Participation < ActiveRecord::Base
   belongs_to :kenshi, inverse_of: :participations
   belongs_to :team
 
-  validates_presence_of :category_id
-  validates :kenshi_id, presence: true, uniqueness: {scope: :category_id}
+  # validates_presence_of :category_id
+  # validates :kenshi_id, presence: true, uniqueness: {scope: :category_id}
   # validates_presence_of :kenshi_id
   validates_presence_of :pool_position, if: lambda{|p| p.pool_number.present?}
   validates_uniqueness_of :category_id, scope: :kenshi_id, if: lambda{|p| p.ronin.blank?}
