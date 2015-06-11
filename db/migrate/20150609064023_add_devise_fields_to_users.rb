69e604cf137a4b701e98c916bb39f45ea3b7a426
Yannis Jaquet <yannis.jaquet@unige.ch>
2015-06-11 09:56:20 +0200

    change migrations to match those on heroku

--- db/migrate/20150609064023_add_devise_fields_to_users.rb
+++ db/migrate/20150609064023_add_devise_fields_to_users.rb
@@ -2,27 +2,27 @@ class AddDeviseFieldsToUsers < ActiveRecord::Migration
   def change
     ## Database authenticatable
     # add_column :users, :email, :string,  null: false, default: ""
-    add_column :users, :encrypted_password, :string, null: false, default: ""
+    # add_column :users, :encrypted_password, :string, null: false, default: ""
 
     # ## Recoverable
-    add_column :users,   :reset_password_token, :string
-    add_column :users, :reset_password_sent_at, :datetime
+    # add_column :users,   :reset_password_token, :string
+    # add_column :users, :reset_password_sent_at, :datetime
 
     # ## Rememberable
-    add_column :users, :remember_created_at, :datetime
+    # add_column :users, :remember_created_at, :datetime
 
     # ## Trackable
-    add_column :users,  :sign_in_count, :integer, default: 0, null: false
-    add_column :users, :current_sign_in_at, :datetime
-    add_column :users, :last_sign_in_at, :datetime
-    add_column :users,   :current_sign_in_ip, :string
-    add_column :users,   :last_sign_in_ip, :string
+    # add_column :users,  :sign_in_count, :integer, default: 0, null: false
+    # add_column :users, :current_sign_in_at, :datetime
+    # add_column :users, :last_sign_in_at, :datetime
+    # add_column :users,   :current_sign_in_ip, :string
+    # add_column :users,   :last_sign_in_ip, :string
 
-    # # Confirmable
-    add_column :users, :confirmation_token, :string
-    add_column :users, :confirmed_at, :datetime
-    add_column :users, :confirmation_sent_at, :datetime
-    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+    # # # Confirmable
+    # add_column :users, :confirmation_token, :string
+    # add_column :users, :confirmed_at, :datetime
+    # add_column :users, :confirmation_sent_at, :datetime
+    # add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
 
     ## Lockable
     # add_column :users, :integer,  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
