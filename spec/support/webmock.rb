c82974d04f38cacac59abebabfe2bebf872c8dbb
Yannis JAQUET <yannisjaquet@mac.com>
2023-05-18 18:45:31 +0200

    Start to add system spec

--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -4,6 +4,16 @@ require "webmock/rspec"
 
 RSpec.configure do |config|
   config.before do
+    # No network activity allowed but we still allow download
+    # of updated chromedriver if needed (useful for system tests in docker)
+    WebMock.disable_net_connect!(
+      allow_localhost: true,
+      allow: [
+        "chromedriver.storage.googleapis.com",
+        ENV["SELENIUM_REMOTE_HOST"]
+      ].compact
+    )
+
     # Slack Notifications
     stub_request(:post, ENV.fetch("SLACK_WEBHOOK"))
       .to_return(status: 200, body: "", headers: {})
---
+++ spec/system/kenshi_form_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe("Show/hide additional info form", :js) do
+  let(:email) { "user@kasaharacup.com" }
+  let(:password) { "password" }
+  let!(:cup) { create(:cup) }
+  let!(:user) { create(:user, email: email, password: password) }
+  let!(:kenshi) { create(:kenshi, cup: cup, user: user) }
+  let!(:product) { create(:product, cup: cup, require_personal_details: true) }
+
+  it do
+    signin_and_visit(user, cup_user_path(cup))
+    # visit new_user_session_path
+    # fill_in "Email", with: email
+    # fill_in "Password", with: password
+    # click_button "Log in"
+    # visit cup_user_path(cup)
+    expect(page).to have_content("#{kenshi.first_name} #{kenshi.last_name}")
+    within("ul[role=list] li") do
+      click_link("Modifier")
+    end
+    expect(page).to have_current_path(edit_cup_kenshi_path(cup, kenshi))
+    expect(page).to have_content("Modifiez l'inscription de #{kenshi.first_name} #{kenshi.last_name}")
+  end
+end
