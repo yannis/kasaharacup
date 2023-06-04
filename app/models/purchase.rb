59ba38456dbcdd7896cc35803971be271079877d
Yannis Jaquet <yannisjaquet@me.com>
2023-06-04 09:41:40 +0200

    Limit number of Kenshi in Dormitory for a Cup (#157)

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -11,6 +11,26 @@ class Purchase < ApplicationRecord
   end
 
   private def in_quota
+    if product&.require_personal_infos
+      dormitory_quota_validation
+    else
+      basic_quota_validation
+    end
+  end
+
+  private def dormitory_quota_validation
+    # We need this validation as in our dormitory, a kenshi cannot reuse the bed
+    # of another kenshi another night.
+    kenshis_in_dormitory_for_cup = Kenshi
+      .joins(purchases: :product)
+      .merge(Product.where(cup_id: product.cup_id, require_personal_infos: true))
+      .distinct
+    return if kenshis_in_dormitory_for_cup.count < ENV.fetch("DORMITORY_QUOTA", 50).to_i
+
+    errors.add(:product_id, :quota_reached)
+  end
+
+  private def basic_quota_validation
     return if product&.quota.nil? || product.purchases.count < product.quota
 
     errors.add(:product_id, :quota_reached)
