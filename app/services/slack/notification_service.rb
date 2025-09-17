39f37d8a2f81421b3d00091eded93f3352fa35f0
Yannis JAQUET <yannisjaquet@mac.com>
2025-09-17 08:22:40 +0200

    Improve Participation model to correctly sort dropdown menus in admin forms
    Signed-off-by: Yannis JAQUET <yannisjaquet@mac.com>

--- app/services/slack/notification_service.rb
+++ app/services/slack/notification_service.rb
@@ -3,6 +3,8 @@
 module Slack
   class NotificationService
     def call(notification:)
+      return if ENV.fetch("SLACK_WEBHOOK", nil).blank?
+
       HTTParty.post(
         ENV.fetch("SLACK_WEBHOOK"),
         body: notification.message.to_json,
