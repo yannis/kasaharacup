1a5ecba3bb460af562b8300b16ae9c7b67a50095
Yannis JAQUET <yannisjaquet@mac.com>
2026-02-26 16:23:14 +0100

    Setup ESLint and fix offenses
    Signed-off-by: Yannis JAQUET <yannisjaquet@mac.com>

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,9 +1,9 @@
+/* global $ */
 //= require active_admin/base
 //= require jquery3
 //= require best_in_place
 //= require best_in_place.purr
 
 $(() => {
-  /* Activating Best In Place */
   $('.best_in_place').best_in_place();
 });
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,7 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from './application';
+import application from './application';
 import NavigationDropdownController
   from '../../components/navigation_dropdown_component/navigation_dropdown_controller';
 import FlashController
